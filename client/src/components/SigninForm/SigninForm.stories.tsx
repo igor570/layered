9219c67be3a1f17402959355714cce54482a4133
jack-surman <jackjimbobsurman@gmail.com>
2024-12-16 23:48:52 +0000

    updated story

--- client/src/components/SigninForm/SigninForm.stories.tsx
+++ client/src/components/SigninForm/SigninForm.stories.tsx
@@ -1,3 +1,9 @@
-// todo install storybook and initialize
-// todo create script for storybook
 // todo ask Igor to setup chromatic for storybook on his user, it needs to be setup by project author
+import type { Meta } from '@storybook/react';
+import SigninForm from './SigninForm.tsx';
+
+const meta: Meta<typeof SigninForm> = {
+    component: SigninForm
+};
+
+export default meta;
