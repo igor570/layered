366f8512ef44a974eef2a05722909e64343f5849
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 13:00:31 +0000

    created error component

--- client/src/components/Error/Error.tsx
+++ client/src/components/Error/Error.tsx
@@ -1,5 +1,9 @@
-function Error(message: string) {
-    return <>{message}</>
+import './Error.scss'
+
+type ErrorProps = { message: string }
+
+function Error({message}: ErrorProps) {
+    return <div data-testid="error" className="error">{ message }</div>
 }
 
 export default Error;
