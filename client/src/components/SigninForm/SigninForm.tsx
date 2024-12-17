4b7554783508232d762c552818ad07358c5dc734
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 01:08:59 +0000

    updated form markup

--- client/src/components/SigninForm/SigninForm.tsx
+++ client/src/components/SigninForm/SigninForm.tsx
@@ -1,13 +1,21 @@
 import './SigninForm.scss';
 
-function SigninForm() {
+export type SigninFormProps = {
+    type: 'registration' | 'login';
+}
+
+function SigninForm({type}: SigninFormProps) {
+    const isLogin = type === 'login';
+    // const formEndpoint = isLogin ? 'login' : 'signup';
+    const formTitle = isLogin ? 'Log In' : 'Sign Up'
+
     return (
         <div className="signin-form">
-            <h2>Sign In</h2>
+            <h2>{ formTitle }</h2>
             <form>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Sign In</button>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <button type="submit">{ formTitle }</button>
             </form>
         </div>
     );
