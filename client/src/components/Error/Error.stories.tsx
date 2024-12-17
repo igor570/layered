80de75faf4fd240d4c4dc3e4e1ac9728efc895ec
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 13:00:49 +0000

    created error story

---
+++ client/src/components/Error/Error.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Error from './Error';
+
+const meta: Meta<typeof Error> = {
+    component: Error,
+}
+
+export default meta;
+
+type Story = StoryObj<typeof Error>;
+
+export const ErrorStory: Story = {
+    render: () => <Error message="An error occurred" />
+}
