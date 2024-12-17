fbac705dcb9abbc9fc408f45a9d4f9ce91468bc8
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 13:07:49 +0000

    updated error mock

---
+++ client/src/components/Error/Error.mock.tsx
@@ -0,0 +1,13 @@
+import { Mocked } from 'vitest';
+
+import './Error';
+
+vi.mock('./Error', () => {
+    return {
+        Error: vi.fn(() => {
+            return <div data-testid='Error'/>;
+        }),
+    };
+});
+
+export const mockedError = Error as Mocked<typeof Error>;
