d1e815b305f7e3df33a231f70c48868ac3af9a2f
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 13:13:27 +0000

    created home story

---
+++ client/src/components/Home/Home.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Home from './Home';
+
+const meta: Meta<typeof Home> = {
+    component: Home,
+}
+
+export default meta;
+
+type Story = StoryObj<typeof Home>;
+
+export const HomeStory: Story = {
+    render: () => <Home/>
+}
