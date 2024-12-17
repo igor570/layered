52363119840927e0961bee6d8477b26077c8a6e4
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 01:17:17 +0000

    remove setup addon

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/components/**/*.mdx", "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
