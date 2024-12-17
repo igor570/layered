f19012fe3cd3f614c296a0ce5fe2ac727914242a
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 01:09:12 +0000

    updated story

--- client/src/components/SigninForm/SigninForm.stories.tsx
+++ client/src/components/SigninForm/SigninForm.stories.tsx
@@ -1,15 +1,33 @@
-// todo ask Igor to setup chromatic for storybook on his user, it needs to be setup by project author
+// todo ask Igor to setup chromatic for storybook on his user, it needs to be setup by project author.
+//  It's good for visual testing
 import { Meta, StoryObj } from '@storybook/react';
 import SigninForm from './SigninForm.tsx';
 
 const meta: Meta<typeof SigninForm> = {
-    component: SigninForm
+    component: SigninForm,
+    argTypes: {
+        type: {
+            options: ['registration', 'login'],
+            control: {
+                type: 'radio'
+            }
+        }
+    }
 };
 
 export default meta;
 
 type Story = StoryObj<typeof SigninForm>;
 
-export const defaultStory: Story = {
-    render: () => <SigninForm />
+export const RegistrationStory: Story = {
+    render: ({type}) => <SigninForm  type={type}/>,
+    args: {
+        type: 'registration'
+    }
+}
+export const LoginStory: Story = {
+    render: ({type}) => <SigninForm  type={type}/>,
+    args: {
+        type: 'login'
+    }
 }
