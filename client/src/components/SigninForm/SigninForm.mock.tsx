e9d0a773f58dbb4401fe0d62d52d633f8d6397b2
jack-surman <jackjimbobsurman@gmail.com>
2024-12-17 13:05:44 +0000

    updated signin form mock

--- client/src/components/SigninForm/SigninForm.mock.tsx
+++ client/src/components/SigninForm/SigninForm.mock.tsx
@@ -1,1 +1,12 @@
-// todo fill out this mock once vitest has been installed
+import { Mocked } from '@storybook/test';
+import SignInForm from './SigninForm.tsx';
+
+vi.mock('./SignInForm', () => {
+    return {
+        SignInForm: vi.fn(() => {
+            return <div data-testid="sign-up-form"/>;
+        })
+    }
+})
+
+export const mockedSignInForm = SignInForm as Mocked<typeof SignInForm>
