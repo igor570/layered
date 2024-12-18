a77b42e2a66108d96099946f6b56566c80bd6978
jack-surman <jackjimbobsurman@gmail.com>
2024-12-18 23:24:40 +0000

    create auth guard function

--- client/src/utils/common.tsx
+++ client/src/utils/common.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Navigate } from 'react-router-dom';
 
 const createTestQueryClient = () =>
   new QueryClient({
@@ -17,4 +17,17 @@ export const hydrateStories = (children: ReactElement): ReactElement => {
       <MemoryRouter>{children}</MemoryRouter>
     </QueryClientProvider>
   );
+};
+
+// todo get correct user type. Should be an obj with isLoggedIn property, then check that in if statement
+type ProtectedRouteProps = {
+  user: any;
+  children: ReactElement;
+  authRoute: string;
+};
+export const ProtectedRoute = ({ user, children, authRoute }: ProtectedRouteProps) => {
+  if (!user) {
+    return <Navigate to={authRoute} replace />;
+  }
+  return children;
 };
