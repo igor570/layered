5896d4774724593f3abd5d6d2a82fb8458061743
jack-surman <jackjimbobsurman@gmail.com>
2024-12-18 15:27:26 +0000

    use updated stories in tests

--- client/src/components/SigninForm/SigninForm.test.tsx
+++ client/src/components/SigninForm/SigninForm.test.tsx
@@ -1,33 +1,13 @@
 import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import QueryClient and QueryClientProvider
 
 import * as SignInFormStories from './SignInForm.stories';
 
 const { RegistrationStory, LoginStory } = composeStories(SignInFormStories);
 
-// Create a new QueryClient instance for each test
-const createTestQueryClient = () =>
-  new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false // Disable retries for testing
-      }
-    }
-  });
-
 describe('SignInForm', () => {
   test('It should render registration form', () => {
-    const queryClient = createTestQueryClient();
-
-    render(
-      <MemoryRouter>
-        <QueryClientProvider client={queryClient}>
-          <RegistrationStory />
-        </QueryClientProvider>
-      </MemoryRouter>
-    );
+    render(<RegistrationStory />);
 
     const signInFormElement = screen.queryByTestId('SignInForm');
     expect(signInFormElement).toBeInTheDocument();
@@ -35,15 +15,7 @@ describe('SignInForm', () => {
   });
 
   test('It should render login form', () => {
-    const queryClient = createTestQueryClient();
-
-    render(
-      <MemoryRouter>
-        <QueryClientProvider client={queryClient}>
-          <LoginStory />
-        </QueryClientProvider>
-      </MemoryRouter>
-    );
+    render(<LoginStory />);
 
     const signInFormElement = screen.queryByTestId('SignInForm');
     expect(signInFormElement).toBeInTheDocument();
