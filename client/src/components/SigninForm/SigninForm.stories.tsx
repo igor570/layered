10ec05e31c3ae9c608e8fbf62151d0837a8c7133
jack-surman <jackjimbobsurman@gmail.com>
2024-12-18 15:54:36 +0000

    updated stories

--- client/src/components/SigninForm/SigninForm.stories.tsx
+++ client/src/components/SigninForm/SigninForm.stories.tsx
@@ -1,19 +1,6 @@
-// todo ask Igor to setup chromatic for storybook on his user, it needs to be setup by project author.
-//  It's good for visual testing
 import { Meta, StoryObj } from '@storybook/react';
 import SignInForm from './SignInForm';
-import { MemoryRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// todo outsource provider to function
-const createTestQueryClient = () =>
-  new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false // Disable retries for testing
-      }
-    }
-  });
-const queryClient = createTestQueryClient();
+import { hydrateStories } from '../../utils/common.tsx';
 
 const meta: Meta<typeof SignInForm> = {
   component: SignInForm,
@@ -32,26 +19,14 @@ export default meta;
 type Story = StoryObj<typeof SignInForm>;
 
 export const RegistrationStory: Story = {
-  render: ({ type }) => (
-    <QueryClientProvider client={queryClient}>
-      <MemoryRouter>
-        <SignInForm type={type} />
-      </MemoryRouter>
-    </QueryClientProvider>
-  ),
+  render: ({ type }) => hydrateStories(<SignInForm type={type} />),
   args: {
     type: 'registration'
   }
 };
 
 export const LoginStory: Story = {
-  render: ({ type }) => (
-    <QueryClientProvider client={queryClient}>
-      <MemoryRouter>
-        <SignInForm type={type} />
-      </MemoryRouter>
-    </QueryClientProvider>
-  ),
+  render: ({ type }) => hydrateStories(<SignInForm type={type} />),
   args: {
     type: 'login'
   }
