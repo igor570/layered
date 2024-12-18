cbd702338aef329bdabf56a37f1e58ba1e75684a
jack-surman <jackjimbobsurman@gmail.com>
2024-12-18 23:25:20 +0000

    protected homepage route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,22 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SigninForm, Home } from './components';
 
 import './index.css';
+import { SigninForm, Home } from './components';
+import { ProtectedRoute } from './utils/common.tsx';
 
 const appClient = new QueryClient();
 
+// todo change user prop to reflect whether user is logged in, currently hardcoded to true
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={appClient}>
       <BrowserRouter>
         <Routes>
-          {/* todo change home route to /, but set up signup form as auth guard */}
-          <Route path='/home' element={<Home />} />
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute user={true} authRoute='/signup'>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path='/signup' element={<SigninForm type='registration' />} />
           <Route path='/login' element={<SigninForm type='login' />} />
-          {/* <Route path="/" element={ <Home> } /> */}
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
