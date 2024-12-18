86a45db0e90d3623cb59865df44c1a3482d10db9
jack-surman <jackjimbobsurman@gmail.com>
2024-12-18 01:00:41 +0000

    included prelim routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import App from './App.tsx';
+import './index.css';
+import Home from './components/Home/Home.tsx';
+import SignInForm from './components/SignInForm/SigninForm.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* todo change home route to default, or make default login page for now */ }
+        <Route path="/home" element={ <Home /> } />
+        <Route path="/signup" element={ <SignInForm type="registration" /> } />
+        {/* <Route path="/" element={ <Home> } /> */ }
+      </Routes>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
