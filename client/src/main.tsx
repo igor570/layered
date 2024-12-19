68cfb857c63b7d6d3906b3075c3e31c3e7c72faa
jack-surman <jackjimbobsurman@gmail.com>
2024-12-19 10:36:53 +0000

    create fallback route for non-existent urls

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { SigninForm, Home } from './components';
@@ -26,6 +26,7 @@ createRoot(document.getElementById('root')!).render(
           />
           <Route path='/signup' element={<SigninForm type='registration' />} />
           <Route path='/login' element={<SigninForm type='login' />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
