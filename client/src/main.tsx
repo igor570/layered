cea529b61441ee9ca8b917c918604d7ca17a2044
jack-surman <jackjimbobsurman@gmail.com>
2024-12-19 12:19:32 +0000

    amended import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { SigninForm, Home } from './components';
-import { ProtectedRoute } from './utils/common.tsx';
+import { ProtectedRoute } from './utils/common';
 
 import './index.css';
 
