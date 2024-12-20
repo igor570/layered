a4150805bb15cc35882c158e34387066f027939d
jack-surman <jackjimbobsurman@gmail.com>
2024-12-20 13:22:20 +0000

    improved input component

--- client/src/components/FormInput/FormInput.tsx
+++ client/src/components/FormInput/FormInput.tsx
@@ -1,15 +1,25 @@
-import { ReactNode } from 'react';
-
 import './FormInput.scss';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+type FormFieldTypes = 'email' | 'password' | 'confirmPassword';
 
 type FormInputProps = {
-  children: ReactNode;
+  type: FormFieldTypes;
+  inputProps?: UseFormRegisterReturn<FormFieldTypes>;
+  placeholder?: string;
 };
 
-export const FormInput = ({ children }: FormInputProps) => {
+export const FormInput = (props: FormInputProps) => {
+  const capitalizeFirstLetter = (word: string) => word.charAt(0).toUpperCase() + word.slice(1);
+
   return (
-    <div data-testid='form-input' className='form-input'>
-      {children}
-    </div>
+    <input
+      data-testid='form-input'
+      {...props?.inputProps}
+      name={props.type}
+      type={props.type}
+      placeholder={props?.placeholder ?? capitalizeFirstLetter(props.type)}
+      className='form-input'
+    />
   );
 };
