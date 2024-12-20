0819fdc58832ac0284656bb33843df729110a5aa
jack-surman <jackjimbobsurman@gmail.com>
2024-12-20 11:40:35 +0000

    amended export

--- client/src/components/FormGroup/FormGroup.tsx
+++ client/src/components/FormGroup/FormGroup.tsx
@@ -4,8 +4,6 @@ type FormGroupProps = {
   children: ReactNode;
 };
 
-const FormGroup = ({ children }: FormGroupProps) => {
+export const FormGroup = ({ children }: FormGroupProps) => {
   return <div className='form-group'>{children}</div>;
 };
-
-export default FormGroup;
