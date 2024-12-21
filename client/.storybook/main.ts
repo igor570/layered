bc40f9ad3ef675fb5f596b581d205eaa49ecaf05
jack-surman <jackjimbobsurman@gmail.com>
2024-12-21 16:59:03 +0000

    amended storybook styling

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/components/**/*.mdx', '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
