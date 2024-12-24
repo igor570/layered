6f29f9ccec5be156ebb2fce6318dbedd6b11da97
Igor Milosavljevic <migor@live.co.uk>
2024-12-24 09:49:52 +0000

    add placeholder navigation

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import './NavBar.scss';
 
 export const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <nav className='nav'>
       <ul className='nav-items'>
-        <a className='nav-item'>Community</a>
-        <a className='nav-item'>Stall</a>
-        <a className='nav-item'>Profile</a>
+        <a className='nav-item' onClick={() => navigate('/community')}>
+          Community
+        </a>
+        <a className='nav-item' onClick={() => navigate('/stall')}>
+          Stall
+        </a>
+        <a className='nav-item' onClick={() => navigate('/profile')}>
+          Profile
+        </a>
       </ul>
     </nav>
   );
