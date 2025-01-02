cb66247881b1489c3d7444426000649b81bf8736
Igor Milosavljevic <migor@live.co.uk>
2025-01-02 10:21:53 +0000

    Added Bento Grid

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -1,17 +1,12 @@
 import './Landing.scss';
 import { NavBar } from '../../components';
-import { LandingCanvas } from './LandingCanvas';
+import { BentoGrid } from './BentoGrid/BentoGrid';
 
 export const Landing = () => {
   return (
     <div className='container'>
       <NavBar />
-      <div data-testid='home' className='home'>
-        <div className='cube-wrapper'>
-          <div className='welcome-text'>LANDING: Say Hello, to easy 3d sharing</div>
-          <LandingCanvas />
-        </div>
-      </div>
+      <BentoGrid />
     </div>
   );
 };
