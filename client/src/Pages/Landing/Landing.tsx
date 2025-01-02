c227d8e38d8983f5f84cac5769d32250cc8c7b43
Igor Milosavljevic <migor@live.co.uk>
2025-01-02 11:11:06 +0000

    added some placeholder styling

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -6,7 +6,11 @@ export const Landing = () => {
   return (
     <div className='container'>
       <NavBar />
-      <BentoGrid />
+      <section>
+        <h1 className='title'>The future of 3D Sharing</h1>
+        <BentoGrid />
+      </section>
+      <section className='placeholder-section'></section>
     </div>
   );
 };
